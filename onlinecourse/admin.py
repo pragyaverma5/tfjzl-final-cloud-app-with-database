90f44f9e0c25dec215d68c7625daff0183aa14c0
pragyaverma5 <verma.pragya.2312@gmail.com>
2026-02-19 00:05:35 +0530

    Fixed admin.py with required inlines

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
-from .models import Course, Lesson, Enrollment, Question, Choice, Submission, Instructor
+from .models import Course, Lesson, Enrollment, Question, Choice, Submission
+
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 1
+
 
 class QuestionInline(admin.TabularInline):
     model = Question
-    extra = 3
+    extra = 1
+
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
+
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+
 
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
@@ -21,28 +26,3 @@ admin.site.register(Enrollment)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
-admin.site.register(Instructor)
-from django.contrib import admin
-from .models import Course, Lesson, Enrollment, Question, Choice, Submission, Instructor
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-class QuestionInline(admin.TabularInline):
-    model = Question
-    extra = 3
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-
-class LessonAdmin(admin.ModelAdmin):
-    inlines = [QuestionInline]
-
-admin.site.register(Course)
-admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Enrollment)
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
-admin.site.register(Submission)
-admin.site.register(Instructor)
