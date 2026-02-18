4fed50da249c7584c1d70cc7a8d30227d501a806
Pragya Verma <162286680+pragyaverma5@users.noreply.github.com>
2026-02-18 22:23:34 +0530

    Register Lesson model in admin panel
    Added Lesson model to admin with LessonAdmin configuration.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from .models import Question, Choice, Submission
+from .models import Question, Choice, Submission, Lesson
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
+admin.site.register(Lesson, LessonAdmin)
